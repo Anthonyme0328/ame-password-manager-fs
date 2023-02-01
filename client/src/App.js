985130d96d9bece5b73328171c17af07014b5439
Anthonyme <anthonymh14@hotmail.com>
2023-02-01 14:15:25 -0500

    bug fix with table

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios'
 
 function App() {
 
   const [password, setPassword] = useState('')
   const [website, setWebsite] = useState('')
+
+  const addPassword = () => {
+    Axios.post("http://localhost:3001/addPassword", {
+      password: password,
+      website: website
+    })
+  }
 
   return (
     <div className="App">
@@ -12,9 +20,11 @@ function App() {
       <div className='addPassword'>
         <input type='text' placeholder='Example. ABC123' onChange={(event) => {
           setPassword(event.target.value)}}/>
+
         <input type='text' placeholder='Example. Instagram' onChange={(event) => {
           setWebsite(event.target.value)}}/>
-        <button>Add Password</button>
+
+        <button onClick={addPassword}>Add Password</button>
       </div>
 
     </div>
--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
 const app = express()
+
 const mysql = require('mysql')
+const cors = require('cors')
+
+
+
 const PORT = 3001
+
+app.use(cors())
+app.use(express.json())
 
 const db = mysql.createConnection({
   user: 'root',
@@ -13,16 +20,17 @@ const db = mysql.createConnection({
 
 app.post('/addPassword', (req, res) => {
 
-  const {password, title} = req.body
+  const {password, website} = req.body
 
   db.query(
-    'INSERT INTO password (password, website) VALUES (?,?)',
-    [password, title],
+    'INSERT INTO passwords (password, website) VALUES (?,?)',
+    [password, website],
     (err, result) => {
       if (err) {
         console.log('there was an issue', err)
       } else {
         res.send('winner winner it worked')
+        console.log(result)
       }
     }
     )
